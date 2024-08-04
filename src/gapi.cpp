392441414451b9269219b40bf1b1a067fab4bffb
HobyProjects <hobbyprojects.dev@gmail.com>
2024-08-04 17:52:02 +0530

    Refactor: Remove unnecessary files and submodules

---
+++ src/gapi.cpp
@@ -0,0 +1,40 @@
+#include "gapi_impl_opengl.hpp"
+
+namespace gapi {
+
+    void shader_container::emplace(const std::shared_ptr<shader>& shader){
+        auto& shader_name = shader->name();
+        gapi_assert(m_shaders.find(shader_name) == m_shaders.end(), "Shader already exists");
+        m_shaders[shader_name] = shader;
+    }
+
+    std::shared_ptr<shader> shader_container::get(const std::string & name) const{
+        auto it = m_shaders.find(name);
+        gapi_assert(it != m_shaders.end(), "Shader not found");
+        return it->second;
+    }
+
+    std::shared_ptr<shader> shader_container::operator[](const std::string & name) const{
+        return get(name);
+    }
+
+    std::shared_ptr<base_api> renderer::m_api = nullptr;
+
+    void renderer::init(){
+        //[FIXME]:This should be platform specific
+        m_api = std::make_shared<opengl::gl_api>();
+        m_api->init();
+    }
+
+    void renderer::clear(){
+        m_api->clear();
+    }
+
+    void renderer::clear_color(float r, float g, float b, float a){
+        m_api->clear_color(r, g, b, a);
+    }
+
+    void renderer::draw(const std::shared_ptr<vertex_array>& va){
+        m_api->draw(va);
+    }
+}
